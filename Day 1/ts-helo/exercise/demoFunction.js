a590bf6450ccc5d7075ed032552c367fbcd57eed
Willy <tukangdonloter@gmail.com>
2023-06-08 06:40:51 +0700

    [Tugas 4] Function

---
+++ Day 1/ts-helo/exercise/demoFunction.js
@@ -0,0 +1,7 @@
+//function types
+function add(a, b) {
+    return a + b;
+}
+function printMessages(msg) {
+    console.log(msg);
+}
---
+++ Day 1/ts-helo/exercise/demoFunction.ts
@@ -0,0 +1,51 @@
+//function types
+function add(a:number, b:number):number|string{
+    return a+b;
+}
+function printMessages(msg:any):void{
+    console.log(msg)
+}
+
+//arrow function
+let logger = function(messages){
+    console.log(messages);
+}
+let doLog = (messages) => {
+    console.log(messages);
+}
+let doLogToo = (messages) => console.log(messages);
+
+//custom type
+let drawPoint = (point)=>{
+    //do something here
+}
+
+drawPoint({
+    x: 1,
+    y: 2
+})
+
+//inline annotation
+let drawPointToo = (point:{x:number, y:number}) => {
+    //do something here
+}
+drawPointToo({
+    x: 1,
+    y: 2
+})
+
+//interface
+interface Point{
+    x:number,
+    y:number
+}
+let drawPointWithInterface = (point:Point)=>{
+    //do something here
+}
+drawPointWithInterface({
+    x: 1,
+    y: 2
+})
+    
+    
+    
