4b86746b247695e0042970a42d0bb951eb96348e
Willy <tukangdonloter@gmail.com>
2023-06-22 06:08:49 +0700

    Hari 12 Linting dan Testing

--- AppBaru/src/app/berita/list-berita/list-berita.component.ts
+++ AppBaru/src/app/berita/list-berita/list-berita.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-list-berita',
   templateUrl: './list-berita.component.html',
   styleUrls: ['./list-berita.component.css']
 })
-export class ListBeritaComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+export class ListBeritaComponent {
 
 }
